44ed43db66bd3d37600daa157ac9ed38df71ab31
Physco Sensei <113809440+physcosensei-OG@users.noreply.github.com>
2022-11-13 21:34:01 +0530

    Update config.py

--- RezeBot/config.py
+++ RezeBot/config.py
@@ -59,6 +59,11 @@ class Config(object):
     AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
+    ALLOW-CHATS = ""
+    HEROKU_API_KEY = ""
+    HEROKU_APP_NAME = ""
+    ARQ_API_KAEY = ""
+    
 
 
 class Production(Config):
