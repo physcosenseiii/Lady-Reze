068f8585c3357615c251990948faf206dc24626e
KIRITO <115206659+KIRITO-40@users.noreply.github.com>
2022-11-13 22:23:28 +0530

    Update config.py

--- RezeBot/config.py
+++ RezeBot/config.py
@@ -25,7 +25,7 @@ class Config(object):
     EVENT_LOGS = -1001545227881  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "postgressql://ovmujxzt:fR8weVTb_O7FCtdsA4_ytkqV0MsWx-Hk@ella.db.elephantsql.com/ovmujxzt" # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "postgresql://ovmujxzt:fR8weVTb_O7FCtdsA4_ytkqV0MsWx-Hk@ella.db.elephantsql.com/ovmujxzt" # needed for any database modules
     REDIS_URI = " "
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
