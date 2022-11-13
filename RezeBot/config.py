e4dd567a9832467f5b1098a852d03bfddefaaf10
Physco Sensei <113809440+physcosensei-OG@users.noreply.github.com>
2022-11-13 21:14:20 +0530

    Update config.py

--- RezeBot/config.py
+++ RezeBot/config.py
@@ -76,6 +76,7 @@ class Config(object):
     MONGO_DB_URI = "mongodb+srv://reze:reze123@rezerobot.p8epm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     REM_BG_API_KEY = "xYCR1ZyK3ZsofjH7Y6hPcyzC"
     HEROKU_API_KEY = ""
+    HEROKU_APP_NAME = ""
     OPENWEATHERMAP_ID = "887da2c60d9f13fe78b0f9d0c5cbaade"
     TEMP_DOWNLOAD_DIRECTORY = ""
 
