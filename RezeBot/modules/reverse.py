0733530f30123586ec3665015f027d52c113b243
Physco Sensei <113809440+physcosenseiii@users.noreply.github.com>
2022-11-20 23:39:52 +0530

    Update reverse.py

--- RezeBot/modules/reverse.py
+++ RezeBot/modules/reverse.py
@@ -1,26 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 import os
 from asyncio import gather, get_running_loop
 from base64 import b64decode
@@ -147,3 +124,15 @@ async def reverse_image_search(client, message: Message):
         text,
         disable_web_page_preview=True,
     )
+
+import requests
+import urllib
+import urllib.request
+@@ -181,7 +479,7 @@ def scam(imgspage, lim):
+   /reverse :- reply to a sticker, or an image to search it!
+Do you know that you can search an image with a link too? /reverse picturelink <amount>.
+"""
+__mod_name__ = "🤍 ʀᴇᴠᴇʀsᴇ 🤍"
+
+REVERSE_HANDLER = DisableAbleCommandHandler(
+    ["pp", "grs" ,"reverse", "p"], reverse, pass_args=True, admin_ok=True
